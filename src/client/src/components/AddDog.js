06012417ff723f5d13da345d5c0eec85436770f3
Andrew Bloyce <bloyce1990@gmail.com>
2020-07-01 22:29:53 +1000

    Added dog breeds

--- src/client/src/components/AddDog.js
+++ src/client/src/components/AddDog.js
@@ -3,59 +3,63 @@ import gql from "graphql-tag";
 import { useMutation } from "react-apollo";
 import { useHistory } from "react-router-dom";
 import { ALL_DOGS } from "./DogList";
+import { dogBreeds } from "../data/dogBreeds";
 
 const ADD_DOG = gql`
-    mutation createDogMutation($name: String!, $breed: String!) {
-        createDog(name: $name, breed: $breed) {
-            id
-            name
-            breed
-        }
-    }
+	mutation createDogMutation($name: String!, $breed: String!) {
+		createDog(name: $name, breed: $breed) {
+			id
+			name
+			breed
+		}
+	}
 `;
 
 const AddDog = () => {
-    const [name, setName] = useState("");
-    const [breed, setBreed] = useState("dog");
-    const [addDog] = useMutation(ADD_DOG, {
-        update(cache, { data: { createDog } }) {
-            const { dogs } = cache.readQuery({ query: ALL_DOGS });
-            cache.writeQuery({
-                query: ALL_DOGS,
-                data: { dogs: [...dogs, createDog] }
-            });
-        }
-    });
-    const history = useHistory();
+	const [name, setName] = useState("Dog");
+	const [breed, setBreed] = useState("dog");
+	const [addDog] = useMutation(ADD_DOG, {
+		update(cache, { data: { createDog } }) {
+			const { dogs } = cache.readQuery({ query: ALL_DOGS });
+			cache.writeQuery({
+				query: ALL_DOGS,
+				data: { dogs: [...dogs, createDog] },
+			});
+		},
+	});
+	const history = useHistory();
 
-    const addNewDog = async () =>
-        addDog({ variables: { name, breed } })
-            .then(() => {
-                history.push("/");
-            })
-            .catch((err) => console.log(err));
+	const addNewDog = async () =>
+		addDog({ variables: { name, breed } })
+			.then(() => {
+				history.push("/");
+			})
+			.catch((err) => console.log(err));
 
-    return (
-        <div>
-            <input
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Dog Name"
-            />
-            <select
-                onChange={(e) => setBreed(e.target.value)}
-                defaultValue="dog"
-            >
-                <option disabled value="dog">
-                    Select a breed
-                </option>
-                <option value="Brittany Spaniel">Brittany Spaniel</option>
-                <option value="Great Dane">Great Dane</option>
-                <option value="Pug">Pug</option>
-            </select>
-            <button onClick={addNewDog}>Add Dog</button>
-        </div>
-    );
+	return (
+		<div>
+			<input
+				type="text"
+				onChange={(e) => setName(e.target.value)}
+				placeholder="Dog Name"
+				defaultValue={name}
+			/>
+			<select
+				onChange={(e) => setBreed(e.target.value)}
+				defaultValue={breed}
+			>
+				<option disabled value="dog">
+					Select a breed
+				</option>
+				{dogBreeds.sort().map((breed) => (
+					<option value={breed} className="capitalize" key={breed}>
+						{breed}
+					</option>
+				))}
+			</select>
+			<button onClick={addNewDog}>Add Dog</button>
+		</div>
+	);
 };
 
 export default AddDog;
