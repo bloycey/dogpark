beb2d9813da3232c018e9df63c2e1110dad510e2
Andrew Bloyce <bloyce1990@gmail.com>
2020-07-11 09:36:51 +1000

    More restructuring

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const { db } = require("./credentials");
 
 const startServer = async () => {
 	const app = express();
+
+	if (process.env.NODE_ENV === "production") {
+		// Serve any static files
+		app.use(express.static(path.join(__dirname, "client/build")));
+
+		// Handle React routing, return all requests to React app
+		app.get("*", function (req, res) {
+			res.sendFile(path.join(__dirname, "client/build", "index.html"));
+		});
+	}
+
 	const server = new ApolloServer({ typeDefs, resolvers });
 
 	server.applyMiddleware({ app });
